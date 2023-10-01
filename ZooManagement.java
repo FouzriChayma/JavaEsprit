73cb9b2b1da2dd7916e026278a4e27733dda0c1d
FouzriChayma <146637048+FouzriChayma@users.noreply.github.com>
2023-10-01 15:25:02 +0200

    Add files via upload

---
+++ ZooManagement.java
@@ -0,0 +1,23 @@
+//Instruction 3
+import java.util.Scanner;
+
+public class ZooManagement {
+    int nbrCages = 20;
+    String zooName = "myZoo";
+
+    public static void main(String[] args) {
+        ZooManagement zm = new ZooManagement();
+        System.out.println(zm.zooName + " comporte " + zm.nbrCages);
+
+        System.out.println("-------------------------------------------------------------");
+        //Instruction 3
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir le nombre de cages souhaité:");
+        int nbrCagesSaisie = sc.nextInt();
+        ZooManagement zm1 = new ZooManagement();
+        zm1.nbrCages = nbrCagesSaisie;
+        //Etape optionelle
+        zm1.zooName = "notMyZoo";
+        System.out.println(zm1.zooName + " comporte " + zm1.nbrCages);
+    }
+}
